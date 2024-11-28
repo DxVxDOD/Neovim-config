0d6fb7d166e773e093974240b65160181785b402
DxVxDOD <orbangdavid24@gmail.com>
2024-11-28 08:52:17 +0000

    neotree telescope treesitter lualine

--- init.lua
+++ init.lua
@@ -1,5 +1,2 @@
-vim.cmd("set relativenumber")
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+require("vim-options")
+require("config.lazy")
